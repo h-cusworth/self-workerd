02a11486e47dc7db504152e235b7d7c697c2acb5
Hannah Cusworth <Hannah.Cusworth@thehutgroup.com>
2025-03-04 10:25:13 +0000

    Remove fly required envs + set DATA_PATH

--- publisher/src/index.ts
+++ publisher/src/index.ts
@@ -13,8 +13,6 @@ const app = new Application({
 
 app.registerEnv({
     DATA_PATH: "required",
-    FLY_AUTH_TOKEN: "required",
-    FLY_WORKER_APP: "required",
 });
 
 app.initialize().then(() => {
